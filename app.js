b6d4dc1a3d8b676acd1eafdbad8f6cdbf6ff23b3
long.chen@magicision.com <long.chen@magicision.com>
2020-09-23 22:17:31 +0800

    feat:增加各类库；简单å®使用router

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const Koa = require('koa');
+const koaRouter = require('koa-router');
 
-const app = new Koa(); // 应用对象程序
+const app = new Koa();
+const router = new koaRouter();
+/*
+*   1. 引入并new一个router对象
+*   2. 编写路由函数
+*   3. 挂在到app上
+* */
+router.get('/classic/latest', (ctx, next) => {
+    ctx.body = {key: 'classic'}
+});
+app.use(router.routes());
+
+/*
+app.use((ctx, next) => {
+    console.log(ctx.path);
+    console.log(ctx.method);
+    if (ctx.path === "/classic/latest" && ctx.method === "GET") {
+        ctx.body = {key: 'classic'}
+    }
+});
+*/
 
 app.listen(3000);
